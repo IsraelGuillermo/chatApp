997f9c0a2ab64b7b9f751c84223ba061203d60ee
Israel Guillermo <israelguillermo22@gmail.com>
2023-04-16 14:53:38 -0500

    updates styles on unselectedState

--- chatApp/client/src/UnselectedState.tsx
+++ chatApp/client/src/UnselectedState.tsx
@@ -1,11 +1,23 @@
-import { Box } from '@material-ui/core';
+import { Box, Typography } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+import ArrowBackIcon from '@material-ui/icons/ArrowBack';
+const useStyles = makeStyles((theme) => ({
+  container: {
+    height: '100vh',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center'
+  }
+}));
 
 export default function UnselectedState() {
+  const classes = useStyles();
   return (
-    <Box className='h-full flex items-center justify-center'>
-      <Box className='text-gray-400'>
-        &larr; Select A Person From the Sidebar
-      </Box>
+    <Box className={classes.container}>
+      <Typography color='textSecondary'>
+        <ArrowBackIcon />
+        Select A Person From the Sidebar
+      </Typography>
     </Box>
   );
 }
