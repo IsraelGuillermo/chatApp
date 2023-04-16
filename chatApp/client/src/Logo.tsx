a80e5db30d23cd4208fa9d3c6cddee20a8cff572
Israel Guillermo <israelguillermo22@gmail.com>
2023-04-16 14:47:13 -0500

    updates styles on logo

--- chatApp/client/src/Logo.tsx
+++ chatApp/client/src/Logo.tsx
@@ -1,10 +1,27 @@
 import { Box } from '@material-ui/core';
 import ChatIcon from '@material-ui/icons/Chat';
+import { makeStyles } from '@material-ui/core/styles';
+const useStyles = makeStyles((theme) => ({
+  container: {
+    color: theme.palette.primary.main,
+    display: 'flex',
+    padding: '1rem',
+    alignItems: 'center'
+  },
+  icon: {
+    marginRight: 8
+  },
+  typography: {
+    fontWeight: 'bolder',
+    fontSize: 24
+  }
+}));
 export default function Logo() {
+  const classes = useStyles();
   return (
-    <Box className='text-blue-600 font-bold flex gap-2 p-4'>
-      <ChatIcon />
-      ChatApp
+    <Box className={classes.container}>
+      <ChatIcon className={classes.icon} />
+      <Box className={classes.typography}>ChatApp</Box>
     </Box>
   );
 }
