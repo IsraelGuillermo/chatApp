2a4e357932814dcc4c497bf57406597f2ebfa037
Israel Guillermo <israelguillermo22@gmail.com>
2023-04-17 14:48:36 -0500

    updates API link

--- chatApp/client/src/App.tsx
+++ chatApp/client/src/App.tsx
@@ -3,7 +3,7 @@ import { UserContextProvider } from './UserContext';
 import Routes from './Routes';
 
 function App() {
-  axios.defaults.baseURL = 'http://localhost:4040';
+  axios.defaults.baseURL = 'https://chat-app-api-blond.vercel.app/';
   axios.defaults.withCredentials = true;
 
   return (
