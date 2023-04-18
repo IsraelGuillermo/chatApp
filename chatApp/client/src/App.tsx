8d0515570939df0da412720c4a23e84e39405f0e
Israel Guillermo <israelguillermo22@gmail.com>
2023-04-17 23:13:45 -0500

    CORS issue fix attempt2

--- chatApp/client/src/App.tsx
+++ chatApp/client/src/App.tsx
@@ -4,7 +4,7 @@ import Routes from './Routes';
 
 function App() {
   axios.defaults.baseURL = 'https://chat-app-api-eight.vercel.app/';
-  axios.defaults.withCredentials = true;
+  // axios.defaults.withCredentials = true;
 
   return (
     <UserContextProvider>
