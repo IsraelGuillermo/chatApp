704418aea8fab2034a21823d6d9f27a0ffc1f93e
Israel Guillermo <israelguillermo22@gmail.com>
2023-04-17 22:40:56 -0500

    vercel issue fix

--- chatApp/server/index.js
+++ chatApp/server/index.js
@@ -21,6 +21,29 @@ app.use('/uploads', express.static(__dirname + '/uploads'));
 app.use(cookieParser());
 app.use(cors());
 
+module.exports = (req, res) => {
+  //set header first to allow request or origin domain (value can be different)
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept'
+  );
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'GET, POST, PUT, PATCH, OPTIONS, DELETE'
+  );
+
+  //---- other code
+
+  //Preflight CORS handler
+  if (req.method === 'OPTIONS') {
+    return res.status(200).json({
+      body: 'OK'
+    });
+  }
+};
+
 async function getUserDataFromRequest(req) {
   return new Promise((resolve, reject) => {
     const token = req.cookies?.token;
