d888afda63b7412cf0e71914e8de09edba90dd81
Israel Guillermo <israelguillermo22@gmail.com>
2023-04-17 23:33:20 -0500

    WS url update 3

--- chatApp/client/src/Chat.tsx
+++ chatApp/client/src/Chat.tsx
@@ -41,7 +41,7 @@ export default function Chat() {
   }, [selectedUserId]);
 
   function connectToWs() {
-    const ws = new WebSocket('ws://chat-app-api-eight.vercel.app/');
+    const ws = new WebSocket('ws://localhost:4040');
     setWs(ws);
     ws.addEventListener('message', handleMessage);
     ws.addEventListener('close', () => {
